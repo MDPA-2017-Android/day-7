f89a4beac543bf1a6dedc13f5c7c2ff754aaf799
albert.denova <albert.denova@king.com>
2018-02-22 13:31:50 +0100

    Add implementation for shared preferences class

--- app/src/main/java/com/lasalle/mdpa/busybudgeter/manager/impl/SharedPreferenceSettingsManagerImpl.java
+++ app/src/main/java/com/lasalle/mdpa/busybudgeter/manager/impl/SharedPreferenceSettingsManagerImpl.java
@@ -10,18 +10,22 @@ public class SharedPreferenceSettingsManagerImpl {
 
 
     public void storeStringSettingValue(String key, String value) {
-        
+        SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+        sharedPreferencesEditor.putString(key, value);
+        sharedPreferencesEditor.commit();
     }
 
     public void storeIntSettingValue(String key, int value) {
-
+        SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+        sharedPreferencesEditor.putInt(key, value);
+        sharedPreferencesEditor.commit();
     }
 
     public String getStringFromKey(String key, String defaultValue) {
-
+        return sharedPreferences.getString(key, defaultValue);
     }
 
     public int getIntFromKey(String key, int defaultValue) {
-
+        return sharedPreferences.getInt(key, defaultValue);
     }
 }
